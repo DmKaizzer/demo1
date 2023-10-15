10c28007a95e46e1a0e0c4bc9a3af85be5087514
Dmitriy.Yamshchikov <Dmitriy.Yamshchikov@fleetcor.ru>
2023-10-15 16:48:20 +0300

    Spring-1

--- src/main/java/com/example/demo/controllers/TestController.java
+++ src/main/java/com/example/demo/controllers/TestController.java
@@ -1,12 +1,8 @@
 package com.example.demo.controllers;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/test")
@@ -14,7 +10,7 @@ public class TestController {
 
     @GetMapping(value = "/get-test-message")
     public String getTestMessage() {
-        return "String test message";
+        return "String test 2 message";
     }
 
 }
